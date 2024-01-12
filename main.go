5cc6dba20b7ca9104730d85304c3302176fe2af9
Paul-developpeur <paulallebee@gmail.com>
2024-01-12 01:55:57 +0100

    pointer

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	var name string = "John"
+	var name2 *string = &name // => pointer to name
+	*name2 = "Jean"           // => change value of name in memory address
 	// or name := "John"
-	fmt.Printf("Hello %s", name)
+	fmt.Printf("Hello %#v : %#v", name, *name2)
 }
